559bdc15f91f0eb77fdbb9873e51ac20ee5316fa
juansesu3 <juanseu3@hotmail.com>
2023-09-14 17:22:23 +0200

    first set up

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // add a rule to handle the canvas.node binary module
+    config.module.rules.push({ test: /\.node$/, use: "raw-loader" });
 
-module.exports = nextConfig
+    // Exclude canvas from beging processed by Next.js in the browser
+    if (!isServer) config.externals.push("canvas");
+    return config;
+  },
+};
+
+module.exports = nextConfig;
