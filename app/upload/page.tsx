243ef0432ced55025a64464a39d066ead9202a2b
juansesu3 <juanseu3@hotmail.com>
2023-09-18 17:15:47 +0200

    FEATURE(Upload page)

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { use, useState } from "react";
+import React, { useState } from "react";
 import { UploadError } from "../types";
 import UploadLayout from "../layouts/UploadLayout";
-import { BiSolidCloudUpload } from "react-icons/bi";
+import { BiLoaderCircle, BiSolidCloudUpload } from "react-icons/bi";
+import { AiOutlineCheckCircle } from "react-icons/ai";
 
 const Upload = () => {
   const router = useRouter();
@@ -14,10 +15,21 @@ const Upload = () => {
   let [error, setError] = useState<UploadError | null>(null);
   let [isUploading, setIsUploading] = useState<boolean>(false);
 
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+
+    if (files && files.length > 0) {
+      const file = files[0];
+      const fileUrl = URL.createObjectURL(file);
+      setFileDisplay(fileUrl);
+      setFile(file);
+    }
+  };
+
   return (
     <>
       <UploadLayout>
-        <div className="w-full mt-[80px] mb-[40px] bg-white shadow-lg rounded-md py-6 md:px-10 px-4">
+        <div className="w-full h-full mt-[80px] mb-[20px] bg-white shadow-lg rounded-md py-6 md:px-10 px-4">
           <div>
             <h1 className="text-[23px] font-semibold ">Upload video</h1>
 
@@ -28,7 +40,7 @@ const Upload = () => {
               <label
                 htmlFor="fileInput"
                 className="md:mx-0 mx-auto mt-4 mb-6 flex flex-col 
-                        items-center justify-center w-full max-w-[260px] h-[470px] 
+                        items-center justify-center w-full max-w-[260px] h-[570px] 
                         text-center p-3 border-2 border-dashed border-gray-300 
                         rounded-lg hover:bg-gray-100 cursor-pointer"
               >
@@ -42,9 +54,63 @@ const Upload = () => {
                   Up to 30 minutes
                 </p>
                 <p className="mt-2 text-gray-400 text-13px] ">Less than 2 GB</p>
+                <label
+                  htmlFor="fileInput"
+                  className="px-2 py-2 mt-8 text-white text-[15px] w-[80%] bg-[#F02C56] rounded-sm cursor-pointer"
+                >
+                  Select file
+                </label>
+                <input
+                  type="file"
+                  id="fileInput"
+                  onChange={onChange}
+                  hidden
+                  accept=".mp4"
+                />
               </label>
             ) : (
-              <div></div>
+              <div
+                className="md:mx-0 mx-auto mt-4 md:mb-12 md:16 flex items-center
+                justify-center w-full max-w-[260px] h-[540px] p-3 rounded-2xl cursor-pointer relative"
+              >
+                {isUploading ? (
+                  <div className="absolute flex items-center justify-center z-20 bg-black w-full h-full rounded-[50px] bg-opacity-50">
+                    <div className="mx-auto flex items-center justify-center gap-1">
+                      <BiLoaderCircle
+                        className="animate-spin"
+                        color="#F12B56"
+                        size={30}
+                      />
+                      <div className="text-white font-bold">Uploading...</div>
+                    </div>
+                  </div>
+                ) : null}
+                <img
+                  className="absolute z-20 pointer-events-none"
+                  src="/images/mobile-case.png"
+                />
+                <img
+                  className="absolute  right-4 bottom-6 z-20"
+                  width="90"
+                  src="/images/tiktok-logo-white.png"
+                />
+                <video
+                  autoPlay
+                  loop
+                  muted
+                  className="absolute rounded-xl object-cover z-10 p-[13px] w-full h-full "
+                  src={fileDisplay}
+                />
+                <div className="absolute -bottom-12 flex items-center justify-between z-50 rounded-xl border w-full p-2 border-gray-300">
+                  <div className="flex items-center truncate">
+                    <AiOutlineCheckCircle size="16" className="min-w-[16px]" />
+                    <p className="text-[11px] pl-1 truncate text-ellipsis">
+                      {file ? file.name : ''} 
+
+                    </p>
+                  </div>
+                </div>
+              </div>
             )}
           </div>
         </div>
